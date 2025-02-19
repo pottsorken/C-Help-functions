c2d7e5f49c0696582b27672897c32a8c5e6e250d
pottsorken <163292800+pottsorken@users.noreply.github.com>
2025-02-19 08:35:44 +0100

    Instructions for use timekeeping

--- timekeeping.h
+++ timekeeping.h
@@ -1,6 +1,17 @@
+
 #include <sys/time.h>
 #include <time.h>
 
+/*
+Will return the number of nano seconds between
+t_start and t_stop and is used for benchmarking
+
+Used like this:
+    clock_gettime(CLOCK_MONOTONIC, &t_start);
+    // What you want to benchmark
+    clock_gettime(CLOCK_MONOTONIC, &t_stop);
+    long result = nano_seconds(&t_start, &t_stop);
+*/
 long nano_seconds(struct timespec *t_start, struct timespec *t_stop)
 {
     return (t_stop->tv_nsec - t_start->tv_nsec) +
